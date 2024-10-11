e5991e7efcdb2da73de07f46df0927f369a03781
Shruti Merchant <shruti.merchant@gmail.com>
2024-10-11 01:16:00 -0700

    fixed analyticsc maybe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Output from './pages/Output';
@@ -11,15 +10,18 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/output" element={<Output />} />
-        <Route path="/previous" element={<QueryList />} />
-        <Route path="/output/:id" element={<HistoricOutput />} />
-        {/* You can add more routes here for additional pages */}
-      </Routes>
-    </Router>
+    <>
+      <Analytics />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/output" element={<Output />} />
+          <Route path="/previous" element={<QueryList />} />
+          <Route path="/output/:id" element={<HistoricOutput />} />
+          {/* You can add more routes here for additional pages */}
+        </Routes>
+      </Router>
+    </>
   )
 }
 
